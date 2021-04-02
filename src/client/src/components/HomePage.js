7ae3f6a6f1d2ed40b85d453b8bc169532d8e26e6
xikylan <lantheman8888@gmail.com>
2021-04-01 22:14:05 -0500

    nice homepage

--- src/client/src/components/HomePage.js
+++ src/client/src/components/HomePage.js
@@ -4,23 +4,34 @@ import { Link } from "react-router-dom";
 
 export default function HomePage() {
   return (
-    <Container bg="white">
-      <Navbar bg="white" expand="lg">
-        <Navbar.Brand as={Link} to="/">
-          milnomen
-        </Navbar.Brand>
-      </Navbar>
-      <Jumbotron style={{ backgroundColor: "white" }}>
-        <h1>Reach 80% fluency</h1>
-        <h2>in just one thousand words</h2>
-        <p>Study the top 1000 words in any language - now with sentences!</p>
-        <p>
-          <Button>Start learning</Button>
-        </p>
-      </Jumbotron>
-      <Container style={{ backgroundColor: "#eee", padding: 10 }}>
-        <p>Check out 800,000+ sentences over 14 languages</p>
+    <>
+      <Container bg="white">
+        <Navbar bg="white" expand="lg">
+          <Navbar.Brand as={Link} to="/">
+            milnomen
+          </Navbar.Brand>
+        </Navbar>
+        <Jumbotron
+          style={{ backgroundColor: "#fff", padding: 70, marginBottom: 0 }}
+        >
+          <h1>Reach 80% fluency</h1>
+          <h2>in just one thousand words</h2>
+          <p style={{ marginTop: 25 }}>
+            Study the top 1000 words in any language - now with sentences!
+          </p>
+          <p style={{ marginTop: 80 }}>
+            <Button style={{ paddingLeft: 20, paddingRight: 20 }}>
+              Start learning
+            </Button>
+          </p>
+        </Jumbotron>
       </Container>
-    </Container>
+
+      <Container fluid style={{ backgroundColor: "#eee" }}>
+        <p style={{ padding: 10, textAlign: "center" }}>
+          Check out 800,000+ sentences over 14 languages
+        </p>
+      </Container>
+    </>
   );
 }
