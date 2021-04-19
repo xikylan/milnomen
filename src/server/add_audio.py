3a721450c62287530147e22a21b4621654df21c9
xikylan <lantheman8888@gmail.com>
2021-04-19 17:09:38 -0500

    added french audio

--- src/server/add_audio.py
+++ src/server/add_audio.py
@@ -3,23 +3,24 @@ from app import Language, Word
 from sys import argv
 import time
 
-code = argv[1]
-lang = Language.query.filter_by(code=code).first()
+name = argv[1]
+lang = Language.query.filter_by(name=name).first()
 words = Word.query.filter_by(language=lang).all()
 
 
 def generate_audio(text):
-    tts = gtts(text=text, lang=code, slow=False)
+    tts = gtts(text=text, lang=lang.code, slow=False)
     assets_path = '../client/src/assets/audio/'
     file_name = text + ".mp3"
-    folder_path = assets_path + code + "/" + file_name
+    folder_path = assets_path + name + "/" + file_name
 
     tts.save(folder_path)
 
 
+start = time.time()
 for word in words:
     text = word.text
-    start = time.time()
     generate_audio(text)
-    end = time.time()
-    print("Generation audio took", end-start, "seconds")
+
+end = time.time()
+print("Generation audio took", end-start, "seconds")
